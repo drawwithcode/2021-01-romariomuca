111922de46de31c8a3b63dcb118624335095d053
romariomuca <romario.muca@gmail.com>
2021-09-29 00:26:19 +0200

    final version

--- sketch.js
+++ sketch.js
@@ -1,13 +1,29 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
+  //IT'S executed only time
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  background("black");
+  frameRate(12); //the sketch will render 12 frames x second
+  angleMode(degrees);
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  //put drawing code here
+  //I can put all the informations about stroke and fill.
+  translate(width / 2, height / 2);
+  // Every frame, it makes a rotation of 3 degrees
+  rotate(frameCount * 0.3);
+
+  noFill();
+
+  stroke(255, 255, 255);
+  ellipse(100, 0, 650, 650);
+  ellipse(100, 0, 450, 450);
+  ellipse(100, 0, 250, 250);
+  ellipse(23, 23, 123, 123);
+
+  stroke(255, 0, 0);
+  ellipse(2, 2, 12, 12);
+
+  stroke(255, 0, 0);
+  ellipse(233, 233, 132, 123);
 }
